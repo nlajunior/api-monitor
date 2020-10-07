423d530707c5052dfced968f1e36f13023c8dfb8
Nilo Jr <nlajunior@gmail.com>
2020-10-07 12:56:13 -0300

    Primeiro commit

---
+++ controller/medicao.py
@@ -0,0 +1,30 @@
+from model.medicao import Medicao
+
+
+class MedicaoController():
+
+    def __init__(self):
+        self.medicao_model = Medicao()
+
+    def get_medicoes(self, limit):
+        medicoes = []
+        try:
+            res = self.medicao_model.get_all(limit=limit)
+            for registro in res:
+                medicoes.append({
+                    'id': registro.id,
+                    'token': registro.token,
+                    'fhr_value': registro.fhr_value,
+                    'duration': registro.duration,
+                    'date_created ': registro.date_created
+                })
+            status = 200
+        except Exception as e:
+            print(e)
+            medicoes = []
+            status = 400
+        finally:
+            return {
+                'result': medicoes,
+                'status': status
+            }
---
+++ model/medicao.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import desc, asc
+from config import app_config, app_active
+
+config = app_config[app_active]
+
+db = SQLAlchemy(config.APP)
+
+class Medicao(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    token = db.Column(db.String(400), unique=True, nullable=False)
+    fhr_value = db.Column(db.Integer, nullable=False)
+    duration = db.Column(db.Integer, nullable=False)   
+    date_created = db.Column(db.DateTime(6), default=db.func.current_timestamp(), nullable=False)
+    active = db.Column(db.Boolean(), default=1, nullable=True)
+
+    def get_all(self, limit=None):
+        try:
+            if limit is None:
+                res = db.session.query(Medicao).order_by(desc(Medicao.date_created)).all()
+            else:
+                res = db.session.query(Medicao).order_by(desc(Medicao.date_created)).limit(limit).all()
+        except Exception as e:
+            res = []
+            print(e)
+        finally:
+            db.session.close()
+            return res    
